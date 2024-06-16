dff0b648ec1fd4592a425b0d4974cb7d685a6c3c
Arman <fakidarman@gmail.com>
2024-06-16 23:02:33 +0600

    implementing miner reward

--- contracts/token.sol
+++ contracts/token.sol
@@ -4,12 +4,24 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract token is ERC20 {
+contract token is ERC20Capped,ERC20Burnable {
     address payable owner;
-    constructor() ERC20("Gold", "GLD") {
+    uint public blockReward;
+    constructor(uint cap, uint reward) ERC20("Arman", "ARM") ERC20Capped(cap *(10 ** decimals())){
         owner = msg.sender;
         _mint(owner, 70000000 * (10**decimals()));
+        reward = reward * (10**decimals());
+    }
+
+    function setReward(uint reward) public onlyOwner{
+        blockReward = reward * (10**decimals());
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "Only Ownder allowed");
+        _;
     }
 }
 
